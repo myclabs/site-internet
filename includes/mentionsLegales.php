c0827e06cc35cbbe13844e1874a3f4627d6b7202
Matthieu Napoli <matthieu@mnapoli.fr>
2013-05-17 16:28:20 +0200

    Clean up V1.0

--- includes/mentionsLegales.php
+++
@@ -1,4 +0,0 @@
-<h2>Mentions légales</h2>
-<ul>
-	<li>Site édité par My C-Sense, SAS au capital de 70000 euros, à capital variable, RCS Versailles 513189498</li>
-</ul>
--- includes/objectifs.php
+++
@@ -1,7 +0,0 @@
-<h2>Nos objectifs</h2>
-<ul>
-	<li><b>Réduire vos coûts et délais</b> d’obtention et d’analyse des données suivies</li>
-	<li>Vous permettre de <b>centraliser et partager</b> l’information en temps réel tout en <b>décentralisant</b> les saisies</li>
-	<li>Vous faire <b>gagner en disponibilité</b>, en souplesse de consultation, en auditabilité des données</li>
-	<li>Faciliter pour vos ressources internes <b>la montée en compétence</b> sur ces sujets</li>
-</ul>
